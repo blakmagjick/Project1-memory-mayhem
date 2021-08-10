be66cb743741112a4093436a4b30b62de1ca42e7
Suzy Q <suzy@iamsuzyq.com>
2021-08-10 16:59:26 -0700

    Found and uploaded pictures, set up an array of the cards

--- js/memory.js
+++ js/memory.js
@@ -1,5 +1,57 @@
 // Project 1 - Memory Mayhem
 
+// Card Options
+const cardsList = [
+    {
+        card: 'wildman',
+        img: 'vanGogh-images/vanGoghWildMan-small'
+    },
+    {
+        card: 'wildman',
+        img: 'vanGogh-images/vanGoghWildMan-small'
+    },
+    {
+        card: 'starryNight',
+        img: 'vanGogh-images/vanGoghStarryNight-small'
+    },
+    {
+        card: 'starryNight',
+        img: 'vanGogh-images/vanGoghStarryNight-small'
+    },
+    {
+        card: 'skeleton',
+        img: 'vanGogh-images/vanGoghSkeleton-small'
+    },
+    {
+        card: 'skeleton',
+        img: 'vanGogh-images/vanGoghSkeleton-small'
+    },
+    {
+        card: 'flowers',
+        img: 'vanGogh-images/vanGoghBeautyEverywhere-small'
+    },
+    {
+        card: 'flowers',
+        img: 'vanGogh-images/vanGoghBeautyEverywhere-small'
+    },
+    {
+        card: 'headshot',
+        img: 'vanGogh-images/vanGoghHeadShot-small'
+    },
+    {
+        card: 'headshot',
+        img: 'vanGogh-images/vanGoghHeadShot-small'
+    },
+    {
+        card: 'mullberrytree',
+        img: 'vanGogh-images/vanGoghMulberryTree-small'
+    },
+    {
+        card: 'mullberrytree',
+        img: 'vanGogh-images/vanGoghMulberryTree-small'
+    }
+]
+
 // Create the Gameboard
 
 // Check for matches
