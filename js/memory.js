04aa6fef28b0e72b861925d8c0a613e76ef26da0
Suzy Q <suzy@iamsuzyq.com>
2021-08-11 20:10:37 -0700

    Took the whole night, but finally got flipTheCard to work

--- js/memory.js
+++ js/memory.js
@@ -1,84 +1,122 @@
+// document.addEventListener('DOMContentLoaded', () => {
+
+
 // Project 1 - Memory Mayhem
 
-// Empty Arrays
-const cardsPicked = []
-const cardsPickedId = []
-
-const memGame = {
+// Empty Array
+    cardsPicked = []
+    cardsPickedId = []
+    matchedCards = []
 // Card Options
+const memGame = {
     cardsList: [
     {
-        card: 'wildman',
-        pic: 'vanGogh-images/vanGoghWildMan-small'
+        card: 'wildman', 
+        pic: '/Users/suzyq/Desktop/GA/sei-bromeliad/projects/project-1-memorymayhem/Project1-memory-mayhem/vanGogh-images/vanGoghWildMan-small.png'
     },
     {
         card: 'wildman',
-        pic: 'vanGogh-images/vanGoghWildMan-small'
+        pic: '/Users/suzyq/Desktop/GA/sei-bromeliad/projects/project-1-memorymayhem/Project1-memory-mayhem/vanGogh-images/vanGoghWildMan-small.png'
     },
     {
         card: 'starryNight',
-        pic: 'vanGogh-images/vanGoghStarryNight-small'
+        pic: '/Users/suzyq/Desktop/GA/sei-bromeliad/projects/project-1-memorymayhem/Project1-memory-mayhem/vanGogh-images/vanGoghStarryNight-small.png'
     },
     {
         card: 'starryNight',
-        pic: 'vanGogh-images/vanGoghStarryNight-small'
+        pic: '/Users/suzyq/Desktop/GA/sei-bromeliad/projects/project-1-memorymayhem/Project1-memory-mayhem/vanGogh-images/vanGoghStarryNight-small.png'
     },
     {
         card: 'skeleton',
-        pic: 'vanGogh-images/vanGoghSkeleton-small'
+        pic: '/Users/suzyq/Desktop/GA/sei-bromeliad/projects/project-1-memorymayhem/Project1-memory-mayhem/vanGogh-images/vanGoghSkeleton-small.png'
     },
     {
         card: 'skeleton',
-        pic: 'vanGogh-images/vanGoghSkeleton-small'
+        pic: '/Users/suzyq/Desktop/GA/sei-bromeliad/projects/project-1-memorymayhem/Project1-memory-mayhem/vanGogh-images/vanGoghSkeleton-small.png'
     },
     {
         card: 'flowers',
-        pic: 'vanGogh-images/vanGoghBeautyEverywhere-small'
+        pic: '/Users/suzyq/Desktop/GA/sei-bromeliad/projects/project-1-memorymayhem/Project1-memory-mayhem/vanGogh-images/vanGoghBeautyEverywhere-small.png'
     },
     {
         card: 'flowers',
-        pic: 'vanGogh-images/vanGoghBeautyEverywhere-small'
+        pic: '/Users/suzyq/Desktop/GA/sei-bromeliad/projects/project-1-memorymayhem/Project1-memory-mayhem/vanGogh-images/vanGoghBeautyEverywhere-small.png'
     },
     {
         card: 'headshot',
-        pic: 'vanGogh-images/vanGoghHeadShot-small'
+        pic: '/Users/suzyq/Desktop/GA/sei-bromeliad/projects/project-1-memorymayhem/Project1-memory-mayhem/vanGogh-images/vanGoghHeadShot-small.png'
     },
     {
         card: 'headshot',
-        pic: 'vanGogh-images/vanGoghHeadShot-small'
+        pic: '/Users/suzyq/Desktop/GA/sei-bromeliad/projects/project-1-memorymayhem/Project1-memory-mayhem/vanGogh-images/vanGoghHeadShot-small.png'
     },
     {
         card: 'mullberrytree',
-        pic: 'vanGogh-images/vanGoghMulberryTree-small'
+        pic: '/Users/suzyq/Desktop/GA/sei-bromeliad/projects/project-1-memorymayhem/Project1-memory-mayhem/vanGogh-images/vanGoghMulberryTree-small.png'
     },
     {
         card: 'mullberrytree',
-        pic: 'vanGogh-images/vanGoghMulberryTree-small'
+        pic: '/Users/suzyq/Desktop/GA/sei-bromeliad/projects/project-1-memorymayhem/Project1-memory-mayhem/vanGogh-images/vanGoghMulberryTree-small.png'
     }
     ],
-// Create the Gameboard
+// Create the Gameboard.
     creatingBoard() {
-        const gameboard = document.querySelector('#board')
+        const gameboard = document.querySelector('.board')
         for (let i = 0; i < this.cardsList.length; i++){
             let cards = document.createElement('img')
             cards.setAttribute('src', 'Front-Back/memoryBack.png') 
-            cards.setAttribute('data-card', i)
-            cards.setAttribute('id', 'gameCard')
-            cards.addEventListener('click', this.flipTheCard())
+            cards.setAttribute('id', i)
+            cards.setAttribute('class', 'cards')
             gameboard.append(cards)  
+            cards.addEventListener('click', (event) => memGame.flipTheCard(event))
             }
         },
-// Flip your card
-    flipTheCard () {
-   },
-// Check for matches
+        // Check for matches
     checkMatches () {
-    } 
+        let cards = document.querySelectorAll('img')
+        let onePicked = this.cardsPickedId[0] 
+        let twoPicked = this.cardsPickedId[1] 
+        if (onePicked === twoPicked){
+            alert('You fournd a match')
+            cards[onePicked].setAttribute('src', '/Users/suzyq/Desktop/GA/sei-bromeliad/projects/project-1-memorymayhem/Project1-memory-mayhem/Front-Back/memoryBlank.png' )
+            cards[twoPicked].setAttribute('src', '/Users/suzyq/Desktop/GA/sei-bromeliad/projects/project-1-memorymayhem/Project1-memory-mayhem/Front-Back/memoryBlank.png' )
+        } else {
+            alert('These do not match')
+            cards[onePicked].setAttribute('src', '/Users/suzyq/Desktop/GA/sei-bromeliad/projects/project-1-memorymayhem/Project1-memory-mayhem/Front-Back/memoryBack.png' )
+            cards[twoPicked].setAttribute('src', '/Users/suzyq/Desktop/GA/sei-bromeliad/projects/project-1-memorymayhem/Project1-memory-mayhem/Front-Back/memoryBack.png')
+        }
+        this.cardsPicked = []
+        this.cardsPickedId = []
+        console.log('Just Checking')
+
+    },
+// Flip your card
+// Store the first event.target.id
+// Store the second event.target.id
+    flipTheCard(event) {
+        let selectedCard = event.target.id
+        cardsPicked.push(this.cardsList[selectedCard].card)
+        console.log(cardsPicked)
+        cardsPickedId.push(selectedCard)
+        console.log(cardsPickedId)
+        
+        event.target.src = this.cardsList[selectedCard].pic
+
+        if (cardsPicked.length === 2){
+            setTimeout(this.checkMatches, 300)
+        } 
+        //   console.log(event.target.id)  
+    }
+     
+    //  cardsList.sort(() => 0.5 - Math.random())
 // Scoring
-
-// Eventlisteners
-
+    }
 // END OF OBJECT, don't put anything after this curly bracket
-}
 
 memGame.creatingBoard()
+
+// Eventlisteners
+// let pushedButton = document.getElementsByClassName('cards')
+// console.log(pushedButton)
+// pushedButton.addEventListener('click', () => memGame.flipTheCard())
+
